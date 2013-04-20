10e1595a101efb5941a879a09ef3011725a41b04
Ramel Coletana <ramzsweet16@gmail.com>
2013-04-20 16:30:27 +0800

    Fix errors for processs.p_hasadvance.php

--- process/p_hasadvance.php
+++ process/p_hasadvance.php
@@ -8,13 +8,8 @@ include "../classes/functions_payment.php";
     $assBalance = $_POST['assBalance'];
     $assAdvance = $_POST['assAdvance'];
     $assOrigAmnt = $_POST['assOrigAmnt'];
-<<<<<<< HEAD
     $assCPayment = $_POST['assCPayment'];
 
     $ha = new payment();
     $ha->p_hasadvance($enrollmentNO,$studentNo,$autoId,$assName,$assessmentNo,$assBalance,$assAdvance,$assOrigAmnt,$assCPayment);
-=======
 
-    $ha = new payment();
-    $ha->p_hasadvance($enrollmentNO,$studentNo,$autoId,$assName,$assessmentNo,$assBalance,$assAdvance,$assOrigAmnt);
->>>>>>> aebbf17b2ff1bff9270d7153bc356d3daa6a08b2
